bcea6dead8d7231b7e9390451436dbf69d6b402a
shaheenshaikh3399 <112388992+shaheenshaikh3399@users.noreply.github.com>
2022-10-04 14:57:10 +0530

    Add files via upload

---
+++ agileProject/src/test/java/io/agileInteligence/com/AgileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileInteligence.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
